1623b2eab79cda78baa6e4320c97ce1512bdef5d
s.jafar.yaghoubi <algo3xp3rt@gmail.com>
2022-10-01 09:19:13 +0330

    Refactor: test integration conflict with create-tb

--- app/app.py
+++ app/app.py
@@ -3,14 +3,14 @@ from decouple import config
 
 from core.urls import routes
 from core.router import Router
+from core.utils import create_tables
 
+create_tables()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config('FLASK_SECRET_KEY')
 router = Router(app, routes)
 
 if __name__ == '__main__':
-    # from core.utils import create_tables
-    # create_tables()
     # app.run(debug=True, port=5005)
     ...
