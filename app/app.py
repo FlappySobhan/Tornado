a3eab96d1040b825dc13afb3f7478d23b67eb704
s.jafar.yaghoubi <algo3xp3rt@gmail.com>
2022-10-07 07:09:43 +0330

    Refactor:  just create fake data in development

--- app/app.py
+++ app/app.py
@@ -5,11 +5,13 @@ from core.urls import routes
 from core.router import Router
 from core.utils import create_tables, generate_data
 
-# Create database tables
-create_tables()
 
-# Generate test data
-generate_data()
+if bool(config('DEVELOPMENT', False)):
+    # Create database tables
+    create_tables()
+
+    # Generate fake data
+    generate_data()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config('FLASK_SECRET_KEY')
